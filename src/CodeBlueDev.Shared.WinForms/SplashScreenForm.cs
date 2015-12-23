fda2d524547c3b89eba8dc9f49e3df7fb841a292
CodeBlue <codebluedev@gmail.com>
2015-12-22 21:01:03 -0700

    Create solution, project, and SplashScreenForm
    Create solution to hold CodeBlueDev.Shared.WinForm project. Add
    SplashScreenForm to project. This class will display a Splash Screen
    to a user for a period of time before closing.

---
+++ src/CodeBlueDev.Shared.WinForms/SplashScreenForm.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+
+namespace CodeBlueDev.Shared.WinForms
+{
+    public partial class SplashScreenForm : Form
+    {
+        public SplashScreenForm()
+        {
+            InitializeComponent();
+        }
+
+        public static void ShowSplashScreen()
+        {
+            
+        }
+
+        public static void CloseSplashScreen()
+        {
+            
+        }
+    }
+}
